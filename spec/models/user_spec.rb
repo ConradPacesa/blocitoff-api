2d2f05f16775fd028c1ebf09cf8606f5175352db
Conrad Pacesa <conradpacesa@gmail.com>
2017-08-04 17:07:53 -0400

    Updated some specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,17 @@ RSpec.describe User, type: :model do
       expect(user).to have_attributes(name: user.name, email: user.email)
     end
   end
+
+  describe "invalid user" do 
+    let(:user_with_invalid_name) { build(:user, name: "") }
+    let(:user_with_invalid_email) { build(:user, email: "") }
+
+    it "should be an invalid user due to blank name" do
+      expect(user_with_invalid_name).to_not be_valid
+    end
+
+    it "should be an invalid user due to blank email" do
+      expect(user_with_invalid_email).to_not be_valid
+    end
+  end
 end
