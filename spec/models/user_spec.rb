6b3cccffde37b81fa38ecd4cd3dd44f14d8bc82a
Conrad Pacesa <conradpacesa@gmail.com>
2017-08-04 16:19:56 -0400

    Updated user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) { create(:user) }
 
-  # it { is_expected.to validate_presence_of(:email) }
-  # it { is_expected.to validate_presence_of(:password) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_presence_of(:password) }
 
   describe "attributes" do
     it 'should have name and email attributes' do
