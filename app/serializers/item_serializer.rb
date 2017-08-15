8a2b591548f3c8601851047a5bb0d035fbc10c80
Conrad Pacesa <conradpacesa@gmail.com>
2017-08-15 19:32:38 -0400

    Created expires_at in Item serializer

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,8 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :expires_at
   has_one :user
+
+  def expires_at
+    object.created_at + 7.days
+  end
 end
