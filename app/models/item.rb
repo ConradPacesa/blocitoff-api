6226e09caa926d183c89ca7dc184cb6d8828c8a4
Conrad Pacesa <conradpacesa@gmail.com>
2017-08-18 13:04:55 -0400

    Modified rake task, todo serializer, model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
+  attr_reader :test_field
+
+  TIME_REMAINING_DAYS = 7
+
+  def expires_at
+    self.created_at + TIME_REMAINING_DAYS.days
+  end
 end
--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -2,7 +2,4 @@ class ItemSerializer < ActiveModel::Serializer
   attributes :id, :name, :expires_at
   has_one :user
 
-  def expires_at
-    object.created_at + 7.days
-  end
 end
