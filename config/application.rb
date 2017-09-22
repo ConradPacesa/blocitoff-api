03030de130715ab18ed2a52735cee47580e9f22c
Conrad Pacesa <conradpacesa@gmail.com>
2017-09-21 21:59:21 -0400

    Updated cors

--- config/application.rb
+++ config/application.rb
@@ -27,10 +27,10 @@ module Myapp
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        origins 'https://peon-leave-44002.netlify.com/'
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :delete]
       end
     end
   end
