a43d7746b2eb6f63509746b0579097d5f9ade876
Conrad Pacesa <conradpacesa@gmail.com>
2017-09-21 22:02:40 -0400

    Updated cors

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module Myapp
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://peon-leave-44002.netlify.com/'
+        origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :delete]
       end
     end
